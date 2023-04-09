9e3bd4fc9c347f863cff8760e152b54a15ea6526
Chandan Anandachari <anandachari.c@northeastern.edu>
2023-04-09 15:06:45 -0400

    Upadted

--- src/main/java/Utils/ReadingData.java
+++ src/main/java/Utils/ReadingData.java
@@ -1,5 +1,7 @@
 package Utils;
 
+import MSTAlgorithms.*;
+
 import java.io.*;
 import java.util.*;
 
@@ -34,17 +36,117 @@ public class ReadingData {
 		String filePath = "crimeSample.csv";
 		List<Data> da = FetchFile(filePath);
 
-		double matrix[][] = new double[da.size()][da.size()];
+		double graph[][] = new double[da.size()][da.size()];
 
 		for (int i = 0; i < da.size(); i++)
 			for (int j = 0; j < da.size(); j++)
-				matrix[i][j] = Utils.HaversineDistance.distance(da.get(i), da.get(j));
+				graph[i][j] = Utils.HaversineDistance.distance(da.get(i), da.get(j));
 
-		for (int i = 0; i < da.size(); i++)
-		{
-			for (int j = 0; j < da.size(); j++)
-				System.out.print(matrix[i][j]);
-			System.out.println();
-		}
+//		Prims pr =new Prims();
+//		List<Edge> li=pr.PrimsAlgorithms(matrix);
+//		int count=0;double sum=0.0;
+//
+//		for(int i=0;i<li.size();i++) {
+//			sum+=li.get(i).getWeight();
+//			System.out.println(li.get(i).getEdge1() + "------->" + li.get(i).getEdge2() + "---------->" + li.get(i).getWeight() + "  " + count++ +"  "+sum);
+//		}
+
+//		Prims Algorithm testing
+//		double graph[][] = new double[][] { { 0, 5, 8, 0},
+//				{ 5, 0, 10, 15 },
+//				{ 8, 10, 0, 20 },
+//				{ 0, 15, 20, 0 },};
+
+//		double graph[][] = {{0, 10, 15, 20},
+//				{10, 0, 35, 25},
+//				{15, 35, 0, 30},
+//				{20, 25, 30, 0}};
+
+//		double graph[][] = {{0, 5, 8, 999},
+//				{5, 0, 10, 15},
+//				{8, 10, 0, 20},
+//				{999, 15, 20, 0}};
+
+//		double graph[][]={{0,60,100,50,90},{60,0,70,40,30},{100,70,0,65,55},{50,40,65,0,110},{90,30,55,110,0}};
+
+//		double graph[][]={{0,7,3,4,5,8},{7,0,8,3,2,1},{3,8,0,8,8,7},{4,3,8,0,7,2},{5,2,8,7,0,6},{8,1,7,2,6,0}};
+
+//		double graph[][]={{0,0,1,0,0},{0,0,1,0,0},{1,1,0,1,0},{0,0,1,0,1},{0,0,0,1,0}};
+
+		Prims pr =new Prims();
+		List<Edge> li=pr.PrimsAlgorithms(graph);
+
+		int count=0;
+
+//		for(int i=0;i<li.size();i++)
+//			System.out.println(li.get(i).getEdge1()+"------->"+li.get(i).getEdge2()+"---------->"+li.get(i).getWeight()+"  "+count++);
+
+		OddVertices ov=new OddVertices();
+
+		List<Integer> oddVert = ov.OddVertex(li, graph.length);
+
+//		for(int i:oddVert)
+//			System.out.println(i);
+
+		double oddVertices[][]=ov.oddVertexCostMatrix(oddVert,graph);
+
+//		for(int i=0;i< oddVertices.length;i++) {
+//			for (int j = 0; j < oddVertices[0].length; j++)
+//				System.out.print(oddVertices[i][j]+"  ");
+//			System.out.println();
+//		}
+
+//		double[][] dataMatrix = {
+//				{70,  40,   20,   55},
+//				{65,  60,   45,   90},
+//				{30,  45,   50,   75},
+//				{25,  30,   55,   40}
+//		};
+// 		2,1,0,3
+
+		Hungarian hungarian=new Hungarian(oddVertices);
+		int orderexecture[] = hungarian.execute();
+
+//		for(int i=0;i<orderexecture.length;i++)
+//			System.out.println(orderexecture[i]);
+
+		List<Edge> matching=hungarian.matching(oddVert,orderexecture,graph);
+//		for(Edge e:matching)
+//			System.out.println(e.getEdge1()+"------>"+e.getEdge2());
+
+		List<Edge> hungarianmatch = new ArrayList<>(li);
+		hungarianmatch.addAll(matching);
+
+//		for(Edge e:eulerian)
+//			System.out.println(e.getEdge1()+" ------> "+e.getEdge2());
+
+
+//		List<Integer> oddVerti = ov.OddVertex(hungarianmatch, graph.length);
+//		for(int i:oddVerti)
+//			System.out.println(i);
+
+//		double g[][]=new double[graph.length][graph.length];
+//		for(Edge e: hungarianmatch)
+//		{
+//			g[e.getEdge1()][e.getEdge2()]=e.getWeight();
+//			g[e.getEdge2()][e.getEdge1()]=e.getWeight();
+//		}
+//
+//		for(int i=0;i< graph.length;i++) {
+//			for (int j = 0; j < graph.length; j++)
+//				System.out.print(g[i][j] + "  ");
+//			System.out.println();
+//		}
+
+
+		EulerianCircuit ec = new EulerianCircuit(hungarianmatch, graph.length);
+
+		List<Integer> ecircuit= ec.EulerianCircuitAlgorithm();
+
+		for(int i:ecircuit)
+			System.out.println(i);
+
+		System.out.println(ec.getCost());
+
 	}
 }
